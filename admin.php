7b253bfa3a6e5b19de3a1023bc6954b2feec23cf
jimDragon <jim.dragons@gmail.com>
2019-05-10 21:13:38 +0300

    Made a separate file for the common functions but it doesnt change the backend/login.php file... I wasnt sure whether to implement it on this too or not..

--- admin.php
+++ admin.php
@@ -1,20 +1,6 @@
 <?php
 
-function getProtocol() {
-    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) {
-        return "https";
-    }
-    else {
-        return "http";
-    }
-}
-
-function redirect($path) {
-    $protocol = getProtocol();
-    $domain = $_SERVER['HTTP_HOST'];
-    header("Location: $protocol://$domain$path");
-    exit;
-}
+include('common-functions.php');
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
